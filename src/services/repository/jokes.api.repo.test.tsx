bbda704ec0a14c27e9d9d0bf2cc4089bb11be257
Covadonga M <cova.millan@gmail.com>
2023-02-26 18:01:14 +0100

    Add jokes api repo file and test

---
+++ src/services/repository/jokes.api.repo.test.tsx
@@ -0,0 +1,118 @@
+import { JokeStructure } from "../../models/jokes";
+import { JokeApiRepo } from "./jokes.api.repo";
+
+const repo = new JokeApiRepo();
+
+describe("Given the Jokes repo", () => {
+  describe("When we use loadJokes", () => {
+    test("Then, if it's ok, it should equal", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue([{ joke: "patata" }]),
+      });
+
+      expect(repo).toBeInstanceOf(JokeApiRepo);
+      const loadJokes = await repo.loadJokes();
+      expect(loadJokes).toEqual([{ joke: "patata" }]);
+    });
+  });
+  describe("When we use loadJokes and fails", () => {
+    test("Then it should throw an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("Error");
+      const loadJokes = repo.loadJokes();
+      await expect(loadJokes).rejects.toThrow();
+    });
+  });
+
+  describe("When it calls the method getJoke", () => {
+    test("Then it should return the item with the same id", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({
+          id: 3,
+          joke: "patata",
+        }),
+      });
+
+      const getJoke = await repo.getJoke(3);
+      expect(getJoke).toEqual({ id: 3, joke: "patata" });
+    });
+  });
+  describe("When getJoke fails", () => {
+    test("Then it should throw an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("Error");
+      const getJoke = repo.getJoke(420);
+      await expect(getJoke).rejects.toThrow();
+    });
+  });
+
+  describe("When we use createJoke", () => {
+    test("Then it should return the value created", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({ id: 1, joke: "patata" }),
+      });
+
+      const createJoke = await repo.createJoke({
+        joke: "patata",
+        isFunny: false,
+        alreadyKnewIt: false,
+      });
+      expect(createJoke).toEqual({ id: 1, joke: "patata" });
+    });
+  });
+  describe("When createJoke fails", () => {
+    test("Then it should throw an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("Error");
+      const createJoke = repo.createJoke({
+        joke: "patata",
+        isFunny: false,
+        alreadyKnewIt: false,
+      });
+      await expect(createJoke).rejects.toThrow();
+    });
+  });
+
+  describe("When we use updateJoke", () => {
+    test("Then it should return the updated value as the user wants", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({ id: 1, joke: "patata" }),
+      });
+
+      const update = await repo.updateJoke({
+        test: "Pepe",
+      } as unknown as Partial<JokeStructure>);
+      expect(update).toEqual({ id: 1, joke: "patata" });
+    });
+  });
+  describe("When update method fails", () => {
+    test("Then it should throw an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("Error");
+      const update = repo.updateJoke({
+        joke: "patata",
+      } as unknown as Partial<JokeStructure>);
+      await expect(update).rejects.toThrow();
+    });
+  });
+
+  describe("When we use deleteJoke", () => {
+    test("Then it should return undefined", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn(),
+      });
+
+      const deleteJoke = await repo.deleteJoke(2);
+      expect(fetch).toHaveBeenCalled();
+      expect(deleteJoke).toBe(undefined);
+    });
+  });
+  describe("When delete method fails", () => {
+    test("Then it should throw an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("Error");
+      const deleteJoke = repo.deleteJoke(1);
+      await expect(deleteJoke).rejects.toThrow();
+    });
+  });
+});
---
+++ src/services/repository/jokes.api.repo.ts
@@ -0,0 +1,71 @@
+import { JokeStructure, ProtoJokeStructure } from "../../models/jokes";
+
+export interface JokeApiRepoStructure {
+  loadJokes(): Promise<JokeStructure[]>;
+  getJoke(id: JokeStructure["id"]): Promise<JokeStructure>;
+  createJoke(joke: ProtoJokeStructure): Promise<JokeStructure>;
+  updateJoke(joke: Partial<ProtoJokeStructure>): Promise<JokeStructure>;
+  deleteJoke(id: JokeStructure["id"]): Promise<void>;
+}
+
+export class JokeApiRepo {
+  url: string;
+  constructor() {
+    this.url = "http://localhost:4200";
+  }
+
+  async loadJokes(): Promise<JokeStructure[]> {
+    const resp = await fetch(this.url);
+    if (!resp.ok)
+      throw new Error("Error Http: " + resp.status + ". " + resp.statusText);
+    const data = (await resp.json()) as JokeStructure[];
+    return data;
+  }
+
+  async getJoke(id: JokeStructure["id"]): Promise<JokeStructure> {
+    const url = this.url + "/" + id;
+    const resp = await fetch(url);
+    if (!resp.ok)
+      throw new Error("Error Http: " + resp.status + ". " + resp.statusText);
+    const data = (await resp.json()) as JokeStructure;
+
+    return data;
+  }
+
+  async createJoke(joke: ProtoJokeStructure): Promise<JokeStructure> {
+    const resp = await fetch(this.url, {
+      method: "POST",
+      body: JSON.stringify(joke),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    if (!resp.ok)
+      throw new Error("Error Http: " + resp.status + ". " + resp.statusText);
+    const data = (await resp.json()) as JokeStructure;
+    return data;
+  }
+
+  async updateJoke(joke: Partial<JokeStructure>): Promise<JokeStructure> {
+    const url = this.url + "/" + joke.id;
+    const resp = await fetch(url, {
+      method: "PATCH",
+      body: JSON.stringify(joke),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    if (!resp.ok)
+      throw new Error("Error Http: " + resp.status + ". " + resp.statusText);
+    const data = (await resp.json()) as JokeStructure;
+    return data;
+  }
+
+  async deleteJoke(id: JokeStructure["id"]): Promise<void> {
+    const url = this.url + "/" + id;
+    const resp = await fetch(url, {
+      method: "DELETE",
+    });
+    if (!resp.ok) throw new Error("Delete not possible");
+  }
+}
